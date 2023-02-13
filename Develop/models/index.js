6d8dabc91b780578fa291664ab2aff3842bb67ce
karguello90 <karguello90@gmail.com>
2023-02-13 13:49:49 -0500

    updating models and index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -14,9 +14,15 @@ Category.hasMany(Product, {
   foreignKey: "category_id"
 })
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
+  through: ProductTag
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  foreignKey: "tag_id",
+  through: ProductTag
+})
 module.exports = {
   Product,
   Category,
